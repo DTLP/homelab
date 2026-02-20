24e243d05f94d2d041d652f29afb42f24205a7b7
Dmitrijs Lapo <dmitry.lapo@hotmail.com>
2026-02-20 18:15:37 +0000

    Fix file names

--- terraform/proxmox/k8s/provider.tf
+++ terraform/proxmox/k8s/provider.tf
@@ -1,12 +1,14 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.96.0"
-    }
-    talos = {
-      source  = "siderolabs/talos"
-      version = "0.9.0"
-    }
+locals {
+  token = file("../secrets/token")
+}
+
+provider "proxmox" {
+  endpoint  = var.pve.endpoint
+  api_token = local.token
+  ssh {
+    agent       = true
+    username    = var.pve.ssh.username
+    private_key = file(var.pve.ssh.private_key_file)
   }
+  insecure = var.pve.insecure
 }
