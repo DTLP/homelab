ebabb0f88335680443b1f732d67d269f662a3ae2
Dmitrijs Lapo <dmitry.lapo@hotmail.com>
2026-02-21 17:29:50 +0000

    Change provider to bpg/proxmox

--- terraform/proxmox/nfs/versions.tf
+++ terraform/proxmox/nfs/versions.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.7.1"
-
   required_providers {
     proxmox = {
-      source  = "telmate/proxmox"
-      version = "3.0.1-rc1"
+      source  = "bpg/proxmox"
+      version = "0.96.0"
     }
   }
 }
