bd3ff22cc8e9518db57ef005cab7394e8b4e677c
Dmitrijs Lapo <dmitry.lapo@hotmail.com>
2026-08-27 19:58:07 +0100

    Drop cluster health check from plans
    data.talos_cluster_health re-runs a full cluster health check with a
    10 minute timeout on every plan, hanging plans on this network. It is
    only needed during the initial bootstrap; talos_cluster_kubeconfig
    already waits for the API server itself.

--- terraform/proxmox/k8s/cluster.tf
+++ terraform/proxmox/k8s/cluster.tf
@@ -25,19 +25,8 @@ resource "talos_machine_bootstrap" "this" {
   node                 = data.talos_client_configuration.this.endpoints[0]
 }
 
-data "talos_cluster_health" "this" {
-  depends_on           = [talos_machine_configuration_apply.this, talos_machine_bootstrap.this]
-  client_configuration = data.talos_client_configuration.this.client_configuration
-  control_plane_nodes  = [for node in var.cluster.nodes : node.ipv4.address if node.type == "controlplane"]
-  worker_nodes         = [for node in var.cluster.nodes : node.ipv4.address if node.type == "worker"]
-  endpoints            = data.talos_client_configuration.this.endpoints
-  timeouts = {
-    read = "10m"
-  }
-}
-
 resource "talos_cluster_kubeconfig" "this" {
-  depends_on           = [talos_machine_bootstrap.this, data.talos_cluster_health.this]
+  depends_on           = [talos_machine_bootstrap.this]
   client_configuration = talos_machine_secrets.this.client_configuration
   node                 = data.talos_client_configuration.this.endpoints[0]
   endpoint             = data.talos_client_configuration.this.endpoints[0]
